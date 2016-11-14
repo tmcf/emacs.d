6bceba38aa28ef9c7535b06095fde2a70bc81eb6
Ted McFadden <ted@errigal751.com>
2016-11-15 09:33:04 +1000

    add tern mode to js

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -4,4 +4,9 @@
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
+(add-to-list 'load-path "/Users/tmcf/DevEnv/tern/emacs/")
+(autoload 'tern-mode "tern.el" nil t)
+
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+
 (provide 'init-local)
